d4dcefe19bab1781fbcd566cb9d7d2cf13644933
Dmitry Agapitov <dmitry.agapitov@blueprintsys.com>
2017-05-25 12:55:09 -0400

    Add sample

---
+++ nsb-ikvm-stackoverflow/Program.cs
@@ -0,0 +1,43 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace nsb_ikvm_stackoverflow
+{
+    public class MyMessage : IMessage
+    {
+        public string Data { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            var endpointConfiguration = new EndpointConfiguration("Samples.SelfHosting");
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            try
+            {
+                Console.WriteLine("\r\nBus created and configured; press any key to stop program\r\n");
+                var myMessage = new MyMessage();
+                await endpointInstance.SendLocal(myMessage)
+                    .ConfigureAwait(false);
+                Console.ReadKey();
+            }
+            finally
+            {
+                await endpointInstance.Stop()
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
